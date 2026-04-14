e8785f6f3d027fcd9a4e128a7acf03155d6f530e
CasualAutopsy <Casual_Autopsy@outlook.com>
2026-04-14 10:24:00 -0400

    Refactoring

--- scripts/parsing.js
+++ scripts/parsing.js
@@ -13,7 +13,7 @@ const {
  * Parses a value into a JSON object/array.
  *
  * @param {string} value - The value to parse.
- * @returns {Object|Array} - The parsed JSON object/array.
+ * @returns {Object|Array<*>} - The parsed JSON object/array.
  */
 export function parseJSON(value) {
     try { // Attempt to parse the value as JSON
@@ -60,7 +60,9 @@ export function parseValue(value) {
  * @returns {*} - The value of the variable.
  */
 function getScopeVar(varName, args) {
+    // @ts-ignore
     if (args._scope.existsVariable(varName)) {
+        // @ts-ignore
         return parseValue(args._scope.getVariable(varName));
     } else { // If the variable doesn't exist, throw an error
         throw new TypeError('No such variable: ' + varName + '(Scope)');
@@ -72,10 +74,12 @@ function getScopeVar(varName, args) {
  *
  * @param {string} varName - The name of the variable to get.
  * @param {Object} args - Slash command arguments.
- * @returns {Object|Array} - The parsed JSON object/array.
+ * @returns {Object|Array<*>} - The parsed JSON object/array.
  */
 function getScopeVarJSON(varName, args) {
+    // @ts-ignore
     if (args._scope.existsVariable(varName)) {
+        // @ts-ignore
         return parseJSON(args._scope.getVariable(varName));
     } else { // If the variable doesn't exist, throw an error
         throw new TypeError('No such variable: ' + varName + '(Scope)');
@@ -87,12 +91,16 @@ function getScopeVarJSON(varName, args) {
  *
  * @param {string} varName - The name of the variable to get.
  * @param {Object} args - Slash command arguments.
- * @returns {{list: Object|Array, setList: function(Object|Array): string}} - The parsed JSON object/array of the variable and a function for updating the variable.
+ * @returns {{list: Object|Array<*>, setList: function(Object|Array<*>): string}} - The parsed JSON object/array of the variable and a function for updating the variable.
  */
 function getMutableScopeVar(varName, args) {
+    // @ts-ignore
     if (args._scope.existsVariable(varName)) {
-        const get = () => parseJSON(args._scope.getVariable(varName));
-        const set = (list) => args._scope.setVariable(varName, JSON.stringify(list));
+        // @ts-ignore
+        const get = () => parseJSON(args._scope.getVariable(varName))
+        // @ts-ignore
+            , set = (/** @type {Object|Array<*>} */ list) => args._scope.setVariable(varName, JSON.stringify(list));
+
         return { list: get(), setList: set };
     } else { // If the variable doesn't exist, throw an error
         throw new TypeError('No such variable: ' + varName + '(Scope)');
@@ -119,7 +127,7 @@ function getLocalVar(varName) {
  * Grab a local variable and parse it into a JSON object/array.
  *
  * @param {string} varName - The name of the variable to get.
- * @returns {Object|Array} - The parsed JSON object/array.
+ * @returns {Object|Array<*>} - The parsed JSON object/array.
  */
 function getLocalVarJSON(varName) {
     const value = getLocalVariable(varName);
@@ -136,14 +144,15 @@ function getLocalVarJSON(varName) {
  * Grab a local variable and parse it into a JSON object/array along with a function for updating the variable.
  *
  * @param {string} varName - The name of the variable to get.
- * @returns {{list: Object|Array, setList: function(Object|Array): string}} - The parsed JSON object/array of the variable and a function for updating the variable.
+ * @returns {{list: Object|Array<*>, setList: function(Object|Array<*>): string}} - The parsed JSON object/array of the variable and a function for updating the variable.
  */
 function getMutableLocalVar(varName) {
     const value = getLocalVariable(varName);
 
     if (value !== '') {
         const get = () => parseJSON(value);
-        const set = (list) => setLocalVariable(varName, JSON.stringify(list));
+        const set = (/** @type {Object|Array<*>} */ list) => setLocalVariable(varName, JSON.stringify(list));
+
         return { list: get(), setList: set };
     } else { // If the variable doesn't exist, throw an error
         throw new TypeError('No such variable: ' + varName + '(Local)');
@@ -170,7 +179,7 @@ function getGlobalVar(varName) {
  * Grab a global variable and parse it into a JSON object/array.
  *
  * @param {string} varName - The name of the variable to get.
- * @returns {Object|Array} The parsed JSON object/array.
+ * @returns {Object|Array<*>} The parsed JSON object/array.
  */
 function getGlobalVarJSON(varName) {
     const value = getGlobalVariable(varName);
@@ -186,14 +195,15 @@ function getGlobalVarJSON(varName) {
  * Grab a global variable and parse it into a JSON object/array along with a function for updating the variable.
  *
  * @param {string} varName - The name of the variable to get.
- * @returns {{list: Object|Array, setList: function(Object|Array): string}} The parsed JSON object/array and a function for updating the variable.
+ * @returns {{list: Object|Array<*>, setList: function(Object|Array<*>): string}} The parsed JSON object/array and a function for updating the variable.
  */
 function getMutableGlobalVar(varName) {
     const value = getGlobalVariable(varName);
 
     if (value !== '') {
-        const get = () => parseJSON(value);
-        const set = (list) => setGlobalVariable(varName, JSON.stringify(list));
+        const get = () => parseJSON(value)
+            , set = (/** @type {Object|Array<*>} */ list) => setGlobalVariable(varName, JSON.stringify(list));
+
         return { list: get(), setList: set };
     } else { // If the variable doesn't exist, throw an error
         throw new TypeError('No such variable: ' + varName + '(Global)');
@@ -205,7 +215,7 @@ function getMutableGlobalVar(varName) {
 /**
  * Parse a value or variable into a value. If the target is a variable name, it will be resolved to its value.
  *
- * @param {string} target - The target variable or value to parse.
+ * @param {*} target - The target variable or value to parse.
  * @param {Object} args - Slash commands arguments.
  * @returns {*} - The parsed value.
  */
@@ -227,9 +237,9 @@ export function parseValueOrVar(target, args) {
 /**
  * Parse a JSON string or variable into a JSON object/array. If the target is a variable name, it will be resolved to its value.
  *
- * @param {string} target - The target variable or value to parse.
+ * @param {*} target - The target variable or value to parse.
  * @param {Object} args - Slash commands arguments.
- * @returns {Object|Array} - The parsed JSON object/array
+ * @returns {Object|Array<*>} - The parsed JSON object/array
  */
 export function parseJSONOrVar(target, args) {
     // Check if the target is a variable name
@@ -250,17 +260,19 @@ export function parseJSONOrVar(target, args) {
  * Parse a value or variable into a value. If the target is a variable name, it will be resolved to its value.
  * Allows mutability of the variable.
  *
- * @param {string} target - The target variable or value to parse.
+ * @param {*} target - The target variable or value to parse.
  * @param {Object} args - Slash commands arguments.
- * @returns {{list: Object|Array, setList: (function(Object|Array): string)}} - The parsed JSON object/array and a function to update the variable.
+ * @returns {{list: Object|Array<*>, setList: (function(Object|Array<*>): string)}} - The parsed JSON object/array and a function to update the variable.
  */
 export function mutableParseValueOrVar(target, args) {
     // Check if the target is a variable name
     const [, prefix, varName] = target.match(/^([.$])?([-_a-zA-Z]+)$/);
 
     if (!varName) { // If it isn't, try to parse it as JSON and create an empty mutation function
-        const get = () => parseJSON(target);
-        const set = () => {};
+        const get = () => parseJSON(target)
+            , set = () => {};
+
+        // @ts-ignore
         return { list: get(), setList: set };
     } else if (!prefix) { // If it is a variable name, check if it has a prefix
         return getMutableScopeVar(varName, args);
