c7ff2627c9097b7c900b773bcdf2d0d5f0ed32f5
CasualAutopsy <Casual_Autopsy@outlook.com>
2026-07-26 11:41:14 -0400

    Refactor and expose with global object.

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-const data = await import('./manifest.json', { with: { type: 'json' } })
-    , loadingOrder = data.default.loading_order;
+// @ts-nocheck
+import { CoercionAndShorthand } from './lib/coercion.js';
+import { MacroCoercionAndShorthand } from './lib/coercion-macros.js';
 
-console.log('[Nox-Lib]Library loaded | Load order: ' + loadingOrder);
+import { MacroHelpers } from './lib/macro-helpers.js';
+
+import { StringOps } from './lib/string-ops.js';
+
+import { KoboAPI } from './lib/kobo-endpoint.js';
+
+globalThis.NoxLib = {
+    CoercionAndShorthand,
+    MacroCoercionAndShorthand,
+
+    MacroHelpers,
+
+    StringOps,
+
+    KoboAPI,
+};
+
+const
+    data = await import('./manifest.json', { with: { type: 'json' } }),
+    loadingOrder = data.default.loading_order;
+
+console.log('[Nox-Lib] Library loaded | Load order: ' + loadingOrder);
